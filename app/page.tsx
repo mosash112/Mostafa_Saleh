9d51d1fc2396ea4e0786ab5a9e25b3aaa889b3ce
mosash <mashalex2000@gmail.com>
2024-03-15 04:00:07 +0200

    Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,38 @@
 // import Image from "next/image";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar"
+import { Navbar } from '../components/Navbar'
 
 export default function Home() {
   return (
-    <main className="flex justify-center items-center h-screen">
-      <h1>My Portfolio</h1>
+    <main className="h-screen">
+      {/* <Menubar>
+        <MenubarMenu>
+          <MenubarTrigger>File</MenubarTrigger>
+          <MenubarContent>
+            <MenubarItem>
+              New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+            </MenubarItem>
+            <MenubarItem>New Window</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Share</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Print</MenubarItem>
+          </MenubarContent>
+        </MenubarMenu>
+      </Menubar> */}
+      <Navbar />
+
+      <div className="flex justify-center items-center h-full">
+        <h1>My Portfolio</h1>
+      </div>
     </main>
   );
 }
