ccbefc9e1712ce5e363e7c0845b1ba53ec7f20aa
mosash <mashalex2000@gmail.com>
2024-05-01 01:42:29 +0300

    adding my work

--- app/work/mern/page.tsx
+++ app/work/mern/page.tsx
@@ -1,22 +1,22 @@
 import { Project, ProjectType } from "@/components/Project";
 
-async function getProps(): Promise<ProjectType[]> {
-    const response = await fetch(process.env.BASE_URL + '/projects.json');
-    const projects: ProjectType[] = await response.json();
-    const filteredProjects = projects.filter(project => project.category === 'mern');
+// async function getProps(): Promise<ProjectType[]> {
+//     const response = await fetch(process.env.BASE_URL + '/projects.json');
+//     const projects: ProjectType[] = await response.json();
+//     const filteredProjects = projects.filter(project => project.category === 'mern');
 
-    return filteredProjects;
-}
+//     return filteredProjects;
+// }
 
 async function Mern() {
-    const projects = await getProps();
+    // const projects = await getProps();
     return (
         <div className="flex flex-col flex-1 m-8 lg:h-fit h-full">
             <h1 className="pb-3 font-bold text-2xl">MERN Projects</h1>
             <p className="pb-3">Profissional website built using React js (vite js, next js) and node js.</p>
-            {projects?.map((project: ProjectType, index: number) => (
+            {/* {projects?.map((project: ProjectType, index: number) => (
                 <Project key={index} {...project} />
-            ))}
+            ))} */}
         </div>
     );
 }
