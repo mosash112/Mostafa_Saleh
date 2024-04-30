8ddabcbef5fe5cf4b1975bfc92e4b9412970f91c
mosash <mashalex2000@gmail.com>
2024-05-01 01:50:35 +0300

    adding my work[0.5]

--- app/work/backend/page.tsx
+++ app/work/backend/page.tsx
@@ -1,22 +1,22 @@
 import { Project, ProjectType } from "@/components/Project";
 
-// async function getProps(): Promise<ProjectType[]> {
-//     const response = await fetch(process.env.BASE_URL + '/projects.json');
-//     const projects: ProjectType[] = await response.json();
-//     const filteredProjects = projects.filter(project => project.category === 'backend');
+async function getProps(): Promise<ProjectType[]> {
+    const response = await fetch(process.env.BASE_URL + '/projects.json');
+    const projects: ProjectType[] = await response.json();
+    const filteredProjects = projects.filter(project => project.category === 'backend');
 
-//     return filteredProjects;
-// }
+    return filteredProjects;
+}
 
 async function Backend() {
-    // const projects = await getProps();
+    const projects = await getProps();
     return (
         <div className="flex flex-col flex-1 m-8 lg:h-fit h-full">
             <h1 className="pb-3 font-bold text-2xl">Back End Projects</h1>
             <p className="pb-3">Backend apis built using node js with express js, SQL and no-SQL databases.</p>
-            {/* {projects?.map((project: ProjectType, index: number) => (
+            {projects?.map((project: ProjectType, index: number) => (
                 <Project key={index} {...project} />
-            ))} */}
+            ))}
         </div>
     );
 }
--- app/work/frontend/page.tsx
+++ app/work/frontend/page.tsx
@@ -1,22 +1,22 @@
 import { Project, ProjectType } from "@/components/Project";
 
-// async function getProps(): Promise<ProjectType[]> {
-//     const response = await fetch(process.env.BASE_URL + '/projects.json');
-//     const projects: ProjectType[] = await response.json();
-//     const filteredProjects = projects.filter(project => project.category === 'frontend');
+async function getProps(): Promise<ProjectType[]> {
+    const response = await fetch(process.env.BASE_URL + '/projects.json');
+    const projects: ProjectType[] = await response.json();
+    const filteredProjects = projects.filter(project => project.category === 'frontend');
 
-//     return filteredProjects;
-// }
+    return filteredProjects;
+}
 
 async function Frontend() {
-    // const projects = await getProps();
+    const projects = await getProps();
     return (
         <div className="flex flex-col flex-1 m-8 lg:h-fit h-full">
             <h1 className="pb-3 font-bold text-2xl">Front End Projects</h1>
             <p className="pb-3">Re-usable frontend built using react js with bootstrap, Tailwind CSS.</p>
-            {/* {projects?.map((project: ProjectType, index: number) => (
+            {projects?.map((project: ProjectType, index: number) => (
                 <Project key={index} {...project} />
-            ))} */}
+            ))}
         </div>
     );
 }
