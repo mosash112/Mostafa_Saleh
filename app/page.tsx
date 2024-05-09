6cea28eeee7e1e410f449682cbc6d2c86b11c70a
mosash <mashalex2000@gmail.com>
2024-05-09 22:14:13 +0300

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="flex justify-center items-evenly h-full">
       <div className="ml-12 flex flex-col flex-1 justify-center">
-        <h1 className="text-5xl font-bold mb-2">Hi, I'm Mostafa Saleh</h1>
+        <h1 className="text-5xl font-bold mb-2">Hi, I am Mostafa Saleh</h1>
         <TitleRotator />
         <p>Dedicated full stack developer with experience in developing high-quality applications and solutions. Adept at all phases of the development lifecycle
           from requirements analysis and design to implementation, testing, and deployment. Passionate about learning new technologies and tools to
