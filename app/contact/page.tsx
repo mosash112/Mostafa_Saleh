242fce243b59d93c139f15e1459f2755b18a15f1
mosash <mashalex2000@gmail.com>
2024-05-13 20:54:34 +0300

    contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -17,11 +17,11 @@ export default function contact() {
             </div>
             <div className="flex-1 w-full border-2 border-foreground rounded-xl p-3">
                 <h1 className="font-semibold text-xl mb-3">Contact directly</h1>
-                <div className="grid grid-cols-[0.2fr_1fr] gap-3 ">
+                <div className="grid md:grid-cols-[0.2fr_1fr] gap-3 ">
                     <h2>Phone: </h2>
                     <h2>+201000518165</h2>
                     <h2>Email: </h2>
-                    <h2>mashalex2000@gmail.com <br /> mostafasaleh11200@gmail.com</h2>
+                    <h2 className="text-sm md:text-md">mashalex2000@gmail.com <br /> mostafasaleh11200@gmail.com</h2>
                 </div>
             </div>
         </div>
