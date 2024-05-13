5c0b87a40921574d579749e3d0a1018db845f3b5
mosash <mashalex2000@gmail.com>
2024-05-13 20:49:57 +0300

    contact page

--- app/contact/layout.tsx
+++ app/contact/layout.tsx
@@ -1,3 +1,3 @@
 export default function ContactLayout({ children, }: { children: React.ReactNode }) {
-    return <section className="flex justify-center items-center w-full h-full bg-background">{children}</section>
+    return <section className="flex justify-center items-center w-full bg-background">{children}</section>
 }
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,8 +3,8 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 
 export default function contact() {
     return (
-        <div className="flex m-4 lg:m-12 w-10/12 items-center justify-evenly gap-10">
-            <div className="flex-1">
+        <div className="flex flex-col md:flex-row m-4 p-4 lg:m-12 w-10/12 items-center justify-evenly gap-10">
+            <div className="flex-1 w-full">
                 <Card className="border-none rounded-2xl ">
                     <CardHeader>
                         <CardTitle>Contact by email</CardTitle>
@@ -15,7 +15,7 @@ export default function contact() {
                     </CardContent>
                 </Card>
             </div>
-            <div className="flex-1 border-2 border-foreground rounded-xl p-3">
+            <div className="flex-1 w-full border-2 border-foreground rounded-xl p-3">
                 <h1 className="font-semibold text-xl mb-3">Contact directly</h1>
                 <div className="grid grid-cols-[0.2fr_1fr] gap-3 ">
                     <h2>Phone: </h2>
